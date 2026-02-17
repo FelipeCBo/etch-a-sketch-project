5e395bf974e200492f57dcd5d9ba885f5c625137
Felipe Correa Bonanni <felipebonanni02@gmail.com>
2026-02-17 13:13:23 -0300

    Do the grid in such a way that the size can be changed

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const container = document.querySelector(".container");
+function createGrid(size) {
+    const CONTAINER = document.getElementById("container");
+    CONTAINER.innerHTML = "";
 
+    const TOTAL_ITEMS = size * size;
 
+    for (let i = 0; i < TOTAL_ITEMS; i++) {
+        const GRID_ITEM = document.createElement("div");
+        GRID_ITEM.classList.add("GRID_ITEM");
+        CONTAINER.appendChild(GRID_ITEM);
+    };
+};
 
-for (let i = 0; i < 256; i++) {
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
-}
+createGrid(16);
 
-const allSquare = document.querySelectorAll(".square");
+const allSquare = document.querySelectorAll(".GRID_ITEM");
 allSquare.forEach((div) => {
     div.addEventListener("mouseenter", () => {
         div.classList.toggle("painted")
